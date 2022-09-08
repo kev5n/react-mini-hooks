21720100030b99f66721ba9cf0a426b8aa78dc16
kev5n <haipen3zhang@outlook.com>
2022-09-08 17:10:41 +0800

    feat add utils areHookInputsEqual

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
-import { hookStore } from "./store";
-import { IHook } from "./types";
+import { hookStore } from './store';
+import { IHook } from './types';
 
 //获取当前hook
 export const updateWorkInProgressHook = () => {
@@ -21,8 +21,8 @@ export const updateWorkInProgressHook = () => {
       if (hookStore.workInProgressHook) {
         hookStore.workInProgressHook.next = hook;
       }
-      hookStore.workInProgressHook = hook;
     }
+    hookStore.workInProgressHook = hook;
   } else {
     hook = hookStore.workInProgressHook as IHook;
     hookStore.workInProgressHook = hookStore.workInProgressHook?.next || null;
@@ -30,3 +30,13 @@ export const updateWorkInProgressHook = () => {
 
   return hook;
 };
+
+export function areHookInputsEqual(nextDeps: Array<any>, prevDeps: Array<any>) {
+  for (let i = 0; i < prevDeps.length && i < nextDeps.length; i++) {
+    if (Object.is(nextDeps[i], prevDeps[i])) {
+      continue;
+    }
+    return false;
+  }
+  return true;
+}
